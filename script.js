600439488eee2cf518e1059dfdff8bc8a1110724
Frogerall <saliticoadrian@gmail.com>
2023-11-24 20:47:15 +0800

    fix navbar bug on desktop

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-const navBtn = document.querySelector(".nav-menu");
-const navClose = document.querySelector(".nav-close");
-const navMenu = document.querySelector("nav");
+function navBar() {
+ const screenWidth = window.innerWidth;
+ const navBtn = document.querySelector(".nav-menu");
+ const navClose = document.querySelector(".nav-close");
+ const navMenu = document.querySelector("nav");
 
-navBtn.addEventListener("click", () => {
- navMenu.classList.toggle("nav-open");
-});
-navClose.addEventListener("click", () => {
- navMenu.classList.remove("nav-open");
-});
+ const toggleNav = () => {
+  navMenu.classList.toggle("nav-open");
+  navBtn.classList.toggle("nav-x");
+  navClose.classList.toggle("nav-toggle");
+ };
+
+ const closeNavOnResize = () => {
+  if (window.innerWidth >= 600) {
+   navMenu.classList.remove("nav-open");
+   navClose.classList.remove("nav-toggle");
+   navBtn.classList.remove("nav-x");
+  }
+ };
+
+ if (screenWidth < 600) {
+  navBtn.addEventListener("click", toggleNav);
+  navClose.addEventListener("click", toggleNav);
+  window.addEventListener("resize", closeNavOnResize);
+ }
+}
+navBar();
